f446ce72334509be8864ec3c28c98549144f704e
Dinesh Bhupathi <dineshbhupathi@Dineshs-MacBook-Air.local>
2025-07-26 16:15:45 +0530

    dev url update

--- UI/src/config.js
+++ UI/src/config.js
@@ -1,7 +1,7 @@
 const local = "http://localhost:8000/api"
 const dev = 'https://empnest.onrender.com/api'
 const config = {
-    BASE_URL: local,
+    BASE_URL: dev,
   };
   
   export default config;
